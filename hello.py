f6d4496047712205c0b1ecc25d5cc3fa48952b87
tanakangururu <tanakangururu00@gmail.com>
2026-05-07 02:31:52 +0200

    add age

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 message = 'Hello world'
 
+age = 20
+year = '2004'
+
 name = input('Please Enter your name: ')
 
 print(f'{message} my name is {name}')
+print(f'Your age is {age} and you were born in {year}')
